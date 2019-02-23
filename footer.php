33f60557a9db3b49ef0e1db229449abd00f44d06
Pranay Das <43801497+pranaydas252@users.noreply.github.com>
2019-02-23 17:49:55 +0530

    Lightbox and AJAX Added

--- footer.php
+++ footer.php
@@ -6,6 +6,7 @@
 <!--Script Tags-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="js/bootstrap.js"></script>
+<script src="js/lightbox-plus-jquery.min.js"></script>
 <script src="js/custom.js"></script>
 <!--Script Tags-->
 
--- header_open.php
+++ header_open.php
@@ -6,5 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<link rel="stylesheet" type="text/css" href="css/lightbox.min.css">
 	<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
